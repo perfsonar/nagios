af02c3315e25a86e461461c3c6043b45acaaa210
Andy Lake <andy@es.net>
2014-04-10 17:11:09 -0400

    Adding new check for ping rtt data. Also adding ability to check_owdelay to look at things other than minimum

--- bin/check_delay.pl
+++ bin/check_delay.pl
@@ -8,12 +8,12 @@ use lib "$Bin/../lib/";
 use Cache::Memcached;
 use Nagios::Plugin;
 use Statistics::Descriptive;
-use perfSONAR_PS::ServiceChecks::OWDelayCheck;
+use perfSONAR_PS::ServiceChecks::DelayCheck;
 use perfSONAR_PS::ServiceChecks::Parameters::LatencyParameters;
 
 use constant DELAY_LABEL => 'ms';
 use constant DELAY_FIELD => 'min_delay';
-use constant DELAY_SCALE => 1000;
+use constant DELAY_SCALE => 1;
 use constant DELAY_STRING => {
     'min_delay' => 'minimum delay',
     'max_delay' => 'maximum delay',
@@ -23,20 +23,11 @@ use constant DELAY_STRING => {
     'p75_delay' => '75th percentile of delay',
     'p95_delay' => '95th percentile of delay',
 };
-use constant LOSS_LABEL => 'pps';
-use constant LOSS_LABEL_LONG => ' packets per session';
-use constant LOSS_PERCENT_LABEL => '%';
-use constant LOSS_FIELD => 'loss';
-use constant LOSS_SCALE => 1;
-use constant LOSS_STRING => 'Loss';
 use constant DEFAULT_DIGITS => 3;
-use constant DEFAULT_MEMD_ADDR => '127.0.0.1:11211';
-use constant DEFAULT_MEMD_EXP => 300;
-use constant DEFAULT_MEMD_COMPRESS_THRESH => 1000000;
 
 my $np = Nagios::Plugin->new( shortname => 'PS_CHECK_OWDELAY',
                               timeout => 60,
-                              usage => "Usage: %s -u|--url <service-url> -s|--source <source-addr> -d|--destination <dest-addr> -b|--bidirectional -l|--loss -p|--percentage --digits <significant-digits> -r <number-seconds-in-past> -w|--warning <threshold> -c|--critical <threshold> -t|timeout <timeout> -m|memcached <server> -e|memcachedexp <expiretime> -q|quantile <quantile>" );
+                              usage => "Usage: %s -u|--url <service-url> -s|--source <source-addr> -d|--destination <dest-addr> -b|--bidirectional --digits <significant-digits> -r <number-seconds-in-past> -w|--warning <threshold> -c|--critical <threshold> -t|timeout <timeout> -q|quantile <quantile>" );
 
 #get arguments
 $np->add_arg(spec => "u|url=s",
@@ -51,18 +42,12 @@ $np->add_arg(spec => "d|destination=s",
 $np->add_arg(spec => "b|bidirectional",
              help => "Indicates that test should be checked in each direction.",
              required => 0 );
-$np->add_arg(spec => "q|quantile=s",
-             help => "The delay metric to analyze. Valid values are min, max, median, p25, p75 and p95. Default is min.",
-             required => 0 );
-$np->add_arg(spec => "l|loss",
-             help => "Look at packet loss instead of delay.",
-             required => 0 );
-$np->add_arg(spec => "p|percentage",
-             help => "Express loss as percentage in output and input parameters are interpreted as percentage.",
-             required => 0 );
 $np->add_arg(spec => "r|range=i",
              help => "Time range (in seconds) in the past to look at data. i.e. 60 means look at last 60 seconds of data.",
              required => 1 );
+$np->add_arg(spec => "q|quantile=s",
+             help => "The delay metric to analyze. Valid values are min, max, median, p25, p75 and p95. Default is min.",
+             required => 0 );
 $np->add_arg(spec => "digits=i",
              help => "Sets the number of significant digits reported after the decimal in results. Must be greater than 0. Defaults to 3.",
              required => 0 );
@@ -72,56 +57,27 @@ $np->add_arg(spec => "w|warning=s",
 $np->add_arg(spec => "c|critical=s",
              help => "threshold of delay (" . DELAY_LABEL . ") that leads to CRITICAL status. In loss mode this is average packets lost as an integer. If -p is specified in addition to -l, then number must be 0-100 (inclusive) and will be interpreted as a percentage.",
              required => 1 );
-$np->add_arg(spec => "m|memcached=s",
-             help => "Address of server in form <address>:<port> where memcached runs. Set to 'none' if want to disable memcached. Defaults to 127.0.0.1:11211",
-             required => 0 );
-$np->add_arg(spec => "e|memcachedexp=s",
-             help => "Time when you want memcached data to expire in seconds. Defaults to lesser of 5 minutes and -r option if not set.",
-             required => 0 );
 $np->getopts;                              
 
 #create client
 my $ma_url = $np->opts->{'u'};
-my $memd_addr = $np->opts->{'m'};
-if(!$memd_addr){
-    $memd_addr = DEFAULT_MEMD_ADDR;
-}
-my $memd  = q{};
-if(lc($memd_addr) ne 'none' ){
-    $memd  = new Cache::Memcached {
-        'servers' => [ $memd_addr ],
-        'debug' => 0,
-        'compress_threshold' => DEFAULT_MEMD_COMPRESS_THRESH,
-    };
-}
-my $memd_expire_time = $np->opts->{'e'};
-if(!$memd_expire_time){
-    $memd_expire_time = DEFAULT_MEMD_EXP;
-    if($np->opts->{'r'} < $memd_expire_time){
-        $memd_expire_time = $np->opts->{'r'};
-    }
-}
 
 #set metric
 my $metric = DELAY_FIELD;
+if($np->opts->{'q'}){
+    $metric = $np->opts->{'q'} . '_delay';
+}
+my $metric_string = DELAY_STRING->{$metric};
+unless($metric_string){
+     $np->nagios_die("Unknown metric " . $metric);
+}
 my $metric_label = DELAY_LABEL;
 my $metric_label_long = DELAY_LABEL;
 my $metric_scale = DELAY_SCALE;
-my $metric_string = DELAY_STRING->{$metric};
-if($np->opts->{'l'}){
-    $metric = LOSS_FIELD;
-    $metric_label = ($np->opts->{'p'} ? LOSS_PERCENT_LABEL : LOSS_LABEL);
-    $metric_label_long = ($np->opts->{'p'} ? LOSS_PERCENT_LABEL : LOSS_LABEL_LONG);
-    $metric_scale = LOSS_SCALE;
-    $metric_string = LOSS_STRING;
-}elsif($np->opts->{'q'}){
-    $metric = $np->opts->{'q'} . '_delay';
-    $metric_string = DELAY_STRING->{$metric};
-    $np->nagios_die("Unknown metric " . $metric) unless($metric_string);
-}
+
 
 #call client
-my $checker = new perfSONAR_PS::ServiceChecks::OWDelayCheck(memd => $memd, memd_expire_time => $memd_expire_time);
+my $checker = new perfSONAR_PS::ServiceChecks::DelayCheck();
 my $parameters = new perfSONAR_PS::ServiceChecks::Parameters::LatencyParameters(
     'ma_url' => $ma_url,
     'source' => $np->opts->{'s'},
@@ -129,8 +85,7 @@ my $parameters = new perfSONAR_PS::ServiceChecks::Parameters::LatencyParameters(
     'time_range' => $np->opts->{'r'},
     'bidirectional' => $np->opts->{'b'},
     'timeout' => $np->opts->{'timeout'},
-    'metric' => $metric,
-    'as_percentage' => $np->opts->{'p'},
+    'metric' => $metric
 );
 my ($result, $stats);
 eval{
@@ -141,7 +96,7 @@ if($@){
 }elsif($result){
     $np->nagios_die($result);
 }elsif($stats->count() == 0 ){
-    my $errMsg = "No one-way delay data returned";
+    my $errMsg = "No delay data returned";
     $errMsg .= " for direction where" if($np->opts->{'s'} || $np->opts->{'d'});
     $errMsg .= " src=" . $np->opts->{'s'} if($np->opts->{'s'});
     $errMsg .= " dst=" . $np->opts->{'d'} if($np->opts->{'d'});
---
+++ lib/perfSONAR_PS/ServiceChecks/DelayCheck.pm
@@ -0,0 +1,80 @@
+package perfSONAR_PS::ServiceChecks::DelayCheck;
+
+use Moose;
+use perfSONAR_PS::Client::Esmond::ApiConnect;
+use perfSONAR_PS::Client::Esmond::ApiFilters;
+
+our $VERSION = 3.4;
+
+extends 'perfSONAR_PS::ServiceChecks::Check';
+
+use constant STAT_MAP => {
+    'min' => 'minimum',
+    'median' => 'median',
+    'max' => 'maximum',
+    'mean' => 'mean',
+    'p25' => 'percentile-25',
+    'p75' => 'percentile-75',
+    'p95' => 'percentile-95'
+};
+
+override 'do_check' => sub {
+    my ($self, $params) = @_;
+    my $stats = Statistics::Descriptive::Sparse->new();
+    my $res = $self->call_ma($params->ma_url, $params->source, $params->destination, $params->time_range, $params->metric, $params->timeout, $stats);
+    return ($res, $stats) if($res);
+    if($params->bidirectional){
+        $res = $self->call_ma($params->ma_url, $params->destination, $params->source, $params->time_range, $params->metric, $params->timeout, $stats);
+        return ($res, $stats) if($res);
+    }
+    return ('', $stats);
+};
+
+sub call_ma {
+    #send request
+    my ($self, $ma_url, $src, $dst, $time_int, $metric, $timeout, $stats) = @_;
+    
+    my $filters = new perfSONAR_PS::Client::Esmond::ApiFilters(timeout => $timeout);
+    my $stat = '';
+    $filters->source($src) if($src);
+    $filters->destination($dst) if($dst);
+    $filters->time_range($time_int) if($time_int);
+    $filters->event_type('histogram-rtt');
+    if($metric =~  /(.+)_delay/){
+        $stat = STAT_MAP->{$1};
+        return "Unrecognized delay stat $1" unless $stat;
+    }else{
+        return "Unrecognized metric $metric";
+    }
+    
+    my $client = new perfSONAR_PS::Client::Esmond::ApiConnect(
+        url => $ma_url,
+        filters => $filters
+    );
+    
+    #parse results
+    my $md = $client->get_metadata();
+    return $client->error if($client->error);
+    unless(scalar(@{$md}) > 0){
+        my $msg = 'Unable to find any tests with data in the given time range';
+        $msg .= " where " if($src || $dst);
+        $msg .= "source is $src" if($src);
+        $msg .= " and " if($src && $dst);
+        $msg .= "destination is $dst" if($dst);
+        return $msg;
+    }
+    foreach my $m(@{$md}){
+        my $et = $m->get_event_type($filters->event_type());
+        my $summ = $et->get_summary('statistics', 0);
+        next unless($summ);
+        my $data = $summ->get_data();
+        return $summ->error if($summ->error);
+        foreach my $d(@{$data}){
+            if($d->val && ref($d->val) eq 'HASH' && exists $d->val->{$stat}){
+                $stats->add_data($d->val->{$stat});
+            }
+        }
+    }
+    
+    return '';  
+}
