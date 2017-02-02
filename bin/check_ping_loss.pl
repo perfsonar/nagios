2df5ccaec5bbd2ab9ffa327dafb1f5719a77b124
Andy Lake <andrewrlake@gmail.com>
2017-02-02 00:45:00 -0500

    Making ping loss check consistent with owdelay loss check

--- bin/check_ping_loss.pl
+++ bin/check_ping_loss.pl
@@ -8,11 +8,12 @@ use lib "$Bin/../lib/";
 use perfSONAR_PS::ServiceChecks::Commands::NagiosEventTypeCmd;
 
 my $cmd = new perfSONAR_PS::ServiceChecks::Commands::NagiosEventTypeCmd(
-    event_type => 'packet-count-lost-bidir',
+    event_type => 'packet-loss-rate-bidir',
     nagios_name => 'PS_CHECK_PING_LOSS',
     metric_name => 'loss',
-    units => 'packets',
-    unit_prefix => ' ',
-    default_digits => 0
+    metric_scale => 100,
+    units => '%',
+    unit_prefix => '',
+    default_digits => 2
 );
 $cmd->run();
